25521cde123e733f932eed45c0042bfee5492335
Prof. Libório Júnior <libajunior@msn.com>
2015-06-15 13:33:27 -0300

    Projeto Pronto

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-angular.module('app.crud', []);
+angular.module('app.crud', ['ngRoute','app.crud-grupo','app.crud-produto'])
+
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.otherwise ({ redirectTo: '/grupos' });
+}]);
--- controller/produtoCtrl.js
+++ controller/produtoCtrl.js
@@ -1,63 +1,90 @@
-function ProdutoController($scope){
-    $scope.produtos = [
-        { nome: 'Monitor Samsungg LED 18,5', qtdade: 0, preco: 350.00, dtinclusao: new Date() },
-        { nome: 'Teclado Microsoft', qtdade: 0, preco: 80.00, dtinclusao: new Date() },
-        { nome: 'Mouse Optico Microsoft', qtdade: 0, preco: 40.00, dtinclusao: new Date() },
-        { nome: 'Impressora HP Multifuncional', qtdade: 0, preco: 250.00, dtinclusao: new Date() },
-        { nome: 'Computador Lenovo 63TW', qtdade: 0, preco: 1200.00, dtinclusao: new Date() },
-        { nome: 'Roteador DLink DIR-610N', qtdade: 0, preco: 70.00, dtinclusao: new Date()},
-        { nome: 'HD Samsung Externo 1TB', qtdade: 0, preco: 340.00, dtinclusao: new Date()}
-    ];
+'use strict';
+
+angular.module('app.crud-produto', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {    
+     $routeProvider.when('/produtos', {
+         templateUrl : 'template/produto.html',
+         controller: 'ProdutoCtrl'
+     });
+}])   
+
+.service('ProdutoSrv',['$window', '$q', function($window, $q){
+    var keyStorage = 'lp3-crud::produto:';
     
-    //Grupos
-    $scope.grupos = [
-        { id: 1, nome: 'Monitor' },
-        { id: 2, nome: 'Hardware' },
-        { id: 3, nome: 'Sofwtare' },
-        { id: 4, nome: 'Periféricos' }
-    ];
-    
-    //Verifica se está selecionado
-    $scope.isSelecionado = function(produtos){
-        
-        return produtos.some(function(item){
-            return item.selecionado;
-        });
-        
+    //Retorna todos os produtos armazenados no localstorage
+    this.all = function() {
+        return angular.fromJson($window.localStorage[keyStorage] || []);
     };
     
+    //Remover um produto no localstorage
+    this.delete = function(produtos) {
+        var deferred = $q.defer();
+        
+        //Filtra, deixando apena os que não serão excluídos
+        var filtrados = produtos.filter(function (produto) {
+            if (!produto.selecionado)
+                return produto;  
+        });
+        
+        //Devolve novamente para a storage
+        $window.localStorage[keyStorage] = angular.toJson(filtrados);
+        deferred.resolve(filtrados);
+        
+        //retorna a promessa
+        return deferred.promise;
+    };
+    
+    //Adicionar um produto no localstorage
+    this.save = function(produto) {
+        var deferred = $q.defer();
+        
+        //Pega os produtos
+        var produtos = this.all();
+        //Adiciona o grupo passado por parâmetro
+        produtos.push(produto);
+        //Devolve novamente para a storage
+        $window.localStorage[keyStorage] = angular.toJson(produtos);
+        deferred.resolve(produto);
+        
+        //retorna a promessa
+        return deferred.promise;
+    };
+}])         
+
+.controller('ProdutoCtrl', ['$scope','$location','GrupoSrv','ProdutoSrv', function ($scope, $location, GrupoSrv, Service) {    
+    $scope.activetab = $location.path();
+                
+    //Declaração da listagem de Produtos e Grupos
+    $scope.produtos = Service.all();    
+    $scope.grupos = GrupoSrv.all();
+    
+    //Verifica se está selecionado
+    $scope.isSelecionado = function(produtos) {
+        return produtos.some(function (produto) {
+            return produto.selecionado;    
+        });
+    };
+    
+    //Ordenar colunas
+    $scope.ordernarPor = function(campo) {
+        $scope.criterioOrdenacao = campo;
+        $scope.direcaoOrdenacao = !$scope.direcaoOrdenacao;
+    };
+ 
     //Adicionar
-    $scope.adicionar = function(produto){
-        $scope.produtos.push(angular.copy(produto));        
-        //Remove o objeto do escope, fazendo com que os campos sejam limpos
-        delete $scope.produto;
-        $scope.produtoForm.$setPristine();
+    $scope.adicionar = function(produto) {
+        Service.save(produto).then(function(data){
+            $scope.produtos.push(angular.copy(produto)); 
+            delete $scope.produto;
+            $scope.produtoForm.$setPristine(); 
+        });
     };
     
     //Remover
-    $scope.remover = function(produtos) {
-        $scope.produtos = produtos.filter(function(item){
-            if (! item.selecionado)
-                return item;
+    $scope.remover = function (produtos) {
+        Service.delete(produtos).then(function(data){
+            $scope.produtos = data;
         });
     };
-}
-angular.module('app.crud').controller('ProdutoCtrl', ProdutoController);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}]);
